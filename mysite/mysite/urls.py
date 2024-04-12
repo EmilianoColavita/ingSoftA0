0192ee621e5ce683fcbd39fbba87850cd8308401
EmilianoColavita <colavitaemiliano1@gmail.com>
2024-04-11 21:55:16 -0300

    [Parte 3.1] desarrollo de vistas

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-
+"""
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,13 +6,15 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
-
-
-
-
-
-
 """
+
+
+
+
+
+
+
+
 from django.contrib import admin
 from django.urls import include, path
 from polls import views  # Importa las vistas de la aplicación polls
@@ -22,5 +24,5 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
-"""
+
 
